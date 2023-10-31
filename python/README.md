860e778c56e555cb41aca1ff0c536e946d62cfa0
Helder Correia <174525+helderco@users.noreply.github.com>
2023-10-31 16:53:15 -0100

    Add python module with http.server and Sphinx
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

---
+++ python/README.md
@@ -0,0 +1,77 @@
+# Dagger Python Module
+
+[![dagger-min-version](https://img.shields.io/badge/dagger%20version-v0.9.2-green)](https://github.com/dagger/dagger/releases/tag/v0.9.2)
+
+Easily manage Python tasks through Dagger.
+
+## Before you start
+
+Set `DAGGER_MODULE` to environment variable to avoid using
+`-m github.com/helderco/daggerverse/python` in every command.
+
+```shell
+export DAGGER_MODULE=github.com/helderco/daggerverse/python
+```
+
+## Commands
+
+### Run a HTTP server
+
+The following will serve the files in the current directory:
+
+```shell
+dagger up http-server -n --src .
+```
+
+Open your browser on [http://0.0.0.0:8000](http://0.0.0.0:8000).
+Click on the [dagger.html](http://0.0.0.0:8000/dagger.html) file.
+
+### Create a new Sphinx project
+
+```shell
+dagger dl sphinx quickstart
+````
+
+This will populate by default the current directory with:
+- `docs`: the Sphinx source files;
+- `public`: the built documentation in HTML;
+
+
+### Open the built documentation in the browser
+
+```shell
+dagger up http-server -n --src public
+```
+
+Open your browser on [http://0.0.0.0:8000](http://0.0.0.0:8000).
+
+### Preview
+
+You can build and preview the documentation with a single command,
+without downloading the built files into your host.
+
+Make some changes to the documentation source files and run:
+
+```shell
+dagger up sphinx preview -n --src docs
+```
+
+## Local development
+
+You don't need to have Python in your host to run this module, but if you
+want to edit the code it's helful to get code inteligence in the IDE.
+
+First, run codegen to get the SDK locally:
+
+```shell
+dagger mod sync
+````
+
+Create a virtual environment, activate it and install the SDK in
+**editable** mode.
+
+```shell
+python -m venv .venv
+source .venv/bin/activate
+pip install -e .
+```
---
+++ python/src/main.py
@@ -0,0 +1,95 @@
+import dagger
+from dagger.mod import Annotated, Arg, Doc, function, object_type
+
+PYTHON_VERSION= "3.12"
+
+
+@function
+def http_server(
+    src: dagger.Directory,
+    bind: Annotated[
+        int,
+        Doc("Bind to this backend port"),
+    ] = 8000,
+    version: Annotated[
+        str,
+        Doc("Use this Python version"),
+        Arg("python_version")
+    ] = PYTHON_VERSION,
+) -> dagger.Service:
+    """Start an HTTP server on a directory."""
+    return (
+        dagger.container()
+        .from_(f"python:{version}-alpine")
+        .with_mounted_directory("/srv", src)
+        .with_workdir("/srv")
+        .with_exec(["python", "-m", "http.server", str(bind)])
+        .with_exposed_port(bind)
+        .as_service()
+    )
+
+
+@object_type
+class Sphinx:
+    version: str = "7.2.6"
+    python_version: str = PYTHON_VERSION
+
+    def base(self) -> dagger.Container:
+        return (
+            dagger.container()
+            .from_(f"python:{self.python_version}-alpine")
+            .with_exec(["pip", "install", f"sphinx=={self.version}"])
+            .with_workdir("/work")
+        )
+
+    @function
+    def quickstart(
+        self,
+        project: Annotated[str, Doc("Project name")] = "Sphinx Demo",
+        author: Annotated[str, Doc("Author name")] = "Kubecon 2023",
+    ) -> dagger.Directory:
+        """Create and build a new Sphinx project."""
+
+        src = (
+            self.base()
+            .with_exec([
+                "sphinx-quickstart", "-q",
+                # Project and author are required flags.
+                "-p", project,
+                "-a", author,
+                ".",
+            ])
+            .directory(".")
+            .without_file("Makefile")
+            .without_file("make.bat")
+        )
+
+        return (
+            dagger.directory()
+            .with_directory("docs", src)
+            .with_directory("public", self.build(src))
+        )
+
+    @function
+    def build(self, src: dagger.Directory) -> dagger.Directory:
+        """Generate HTML documentation from source files."""
+        return (
+            self.base()
+            .with_mounted_directory("/src", src)
+            .with_exec(["sphinx-build", "/src", "."])
+            .directory(".")
+        )
+
+    @function
+    def preview(self, src: dagger.Directory) -> dagger.Service:
+        """Build and preview documentation in a browser."""
+        return http_server(self.build(src))
+
+
+@function
+def sphinx(version: Annotated[str | None, Doc("Sphinx version")] = None) -> Sphinx:
+    """Create and build documentation with Sphinx."""
+    kwargs = {}
+    if version is not None:
+        kwargs["version"] = version
+    return Sphinx(**kwargs)
