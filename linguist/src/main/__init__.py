d678847b11ec519333d3c0c45f320a02399be3ea
Helder Correia <174525+helderco@users.noreply.github.com>
2024-03-10 11:42:39 -0100

    linguist: Update docs and pin packages
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- linguist/src/main/__init__.py
+++ linguist/src/main/__init__.py
@@ -1,9 +1,17 @@
-"""A tool for analyzing the languages used in a git repository"""
+"""A tool for analyzing the languages used in a git repository
+
+Mostly used for confirming that a .gitattributes file correctly marks a given
+file as being generated.
+
+Note: there's an issue when cache is cold. The linguist repo is pulled with
+dag.git() which also pulls in the submodules. Every language in Linguist is
+in an external repo, commited as a submodule, so it takes approximately 10
+minutes to pull the entire thing.
+"""
 from typing import Annotated, Final
 
 import dagger
 from dagger import Doc, dag, field, function, object_type
-
 
 REPO: Final[str] = "github.com/github-linguist/linguist"
 DEFAULT_COMMIT: Final[str] = "559a6426942abcae16b6d6b328147476432bf6cb"
@@ -31,10 +39,11 @@ class Linguist:
     @function
     async def run(
         self,
-        file: Annotated[str, "Path to a single file to analyze"] = "",
-        rev: Annotated[str, "The git revision to analyze"] = "",
-        breakdown: Annotated[bool, "Show the breakdown of files by language"] = False,
-        as_json: Annotated[bool, "Output the Linguist data in JSON format"] = False,
+        file: Annotated[str, Doc("Path to a single file to analyze")] = "",
+        *,
+        rev: Annotated[str, Doc("The git revision to analyze")] = "",
+        breakdown: Annotated[bool, Doc("Show the breakdown of files by language")] = False,
+        as_json: Annotated[bool, Doc("Output the Linguist data in JSON format")] = False,
     ) -> str:
         """Run the github-linguist tool on the source code"""
         args = ["github-linguist"]
